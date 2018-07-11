c6608a6f5d982dbead3e24809d05027e47022e35
wangligit <wangligit@users.noreply.github.com>
2018-07-11 17:50:24 +0800

    Update Constants.m

--- VoipSDK/Constants.m
+++ VoipSDK/Constants.m
@@ -10,8 +10,8 @@
 
 @implementation Constants
 
- NSString * const SIP_SERVER = @"uc.w-ie.com.cn";
- NSString * const SIP_PROXY = @"101.132.33.115";
+ NSString * const SIP_SERVER = @"";
+ NSString * const SIP_PROXY = @"";
  NSString * const PREFIX_NAME = @"CC_SOFTPHONE";
  NSString * const AES_KEY = @"ahD3gjUiwuW87Dkj";
  NSString * const AES_IV = @"0000000000000000";
